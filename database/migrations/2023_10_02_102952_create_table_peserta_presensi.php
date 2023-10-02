24a8acbbd5be5e73a709bf5d6e2c07c1f1d5ee8a
aguskhoiruddin <agus.khoiruddin@gmail.com>
2023-10-02 17:43:36 +0700

    qrcode dan presensi

---
+++ database/migrations/2023_10_02_102952_create_table_peserta_presensi.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+
+        Schema::create('event_qrcodes', function (Blueprint $table) {
+            $table->id(); 
+            $table->string('judul', 100); 
+            $table->unsignedBigInteger('event_id');  
+            $table->string('qr', 64)->unique();
+
+            $table->foreign('event_id')->references('id')->on('events');
+
+             
+        });
+
+        Schema::create('event_peserta_presensi', function (Blueprint $table) {
+            $table->id();  
+            $table->unsignedBigInteger('qrcode_id');  
+            $table->unsignedBigInteger('user_id');  
+            $table->timestamp('jam_presensi');
+
+            $table->foreign('qrcode_id')->references('id')->on('event_qrcodes'); 
+            $table->foreign('user_id')->references('id')->on('users'); 
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('event_peserta_presensi');
+        Schema::dropIfExists('event_qrcodes');
+    }
+};
